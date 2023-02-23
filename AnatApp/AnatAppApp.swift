5ebf4f1845230fdaadebef912d87edb1a1355be9
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2023-02-23 15:10:07 +0300

    Initial Commit

---
+++ AnatApp/AnatAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  AnatAppApp.swift
+//  AnatApp
+//
+//  Created by Mashael Alghunaim on 03/08/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct AnatAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
