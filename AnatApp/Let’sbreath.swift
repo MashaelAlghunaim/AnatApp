10d92bcd71eca7f4233973c94acba2b941fbc567
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2023-02-26 13:08:28 +0300

    .

--- AnatApp/Let’sbreath.swift
+++ AnatApp/Let’sbreath.swift
@@ -18,18 +18,20 @@ struct Let_sbreath: View {
                     .padding(.bottom, 450)
                     .foregroundColor(Color("blue"))
                 
-                Text("Okay")
-                    .font(.title)
-                    .frame(maxWidth: 350 , maxHeight: 48)
-                    .foregroundColor(.white)
-                    .background(Color("blue"))
-                    .clipShape(RoundedRectangle(cornerRadius: 10))            }
+//                Text("Okay")
+//                    .font(.title)
+//                    .frame(maxWidth: 350 , maxHeight: 48)
+//                    .foregroundColor(.white)
+//                    .background(Color("blue"))
+//                .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+            }
             
         }
-       
-        }
+        
     }
-    
+}
+
 
 
 struct Let_sbreath_Previews: PreviewProvider {
@@ -43,17 +45,17 @@ struct Letsbreath : View {
         ZStack{
             ForEach(1...8,id: \.self){index in
                 Circle()
-                                .fill(Color("blue").gradient.opacity(0.3))
-                                .frame(width: 150, height: 150)
-                                .scaleEffect(self.animate ? 1:0)
-                                // 150 / 9 -> 75
-                                .offset(x: 75)
-                                .rotationEffect(.init(degrees: Double(index) * 45))
+                    .fill(Color("blue").gradient.opacity(0.3))
+                    .frame(width: 150, height: 150)
+                    .scaleEffect(self.animate ? 1:0)
+                // 150 / 9 -> 75
+                    .offset(x: 75)
+                    .rotationEffect(.init(degrees: Double(index) * 45))
             }
-                                .onAppear {
-                                    self.animate.toggle()
-                                }
-                                .animation(Animation.linear(duration: 2.9).repeatForever(autoreverses: true))
+            .onAppear {
+                self.animate.toggle()
+            }
+            .animation(Animation.linear(duration: 2.9).repeatForever(autoreverses: true))
             
         }
     }
--- AnatApp/groundingPage.swift
+++ AnatApp/groundingPage.swift
@@ -11,64 +11,78 @@ struct groundingPage: View {
     
     @State var text = "5 things you can see"
     @State var mainText = "Look around you for..."
-    @State var showingSheet = false
+    @State var settings = "Settings"
+    
+    @State var isPrsnt = false
+    
     var body: some View {
-        
         NavigationView{
-            
             ZStack{
-              
-                HStack{
-                    Button( action:{
-                        
-                    })
+                
+                VStack(spacing: 10){
                     
-                    {
-                        Ellipse().fill(Color("blue")).frame(width: 10, height: 10)
-                        Ellipse().fill(Color("blue")).frame(width: 10, height: 10)
-                        Ellipse().fill(Color("blue")).frame(width: 10, height: 10)
+                    Button(action: {
+                        isPrsnt = true
+                    },label:{ Image(systemName: "gear").resizable().frame(width: 30, height: 30)
+                        .foregroundColor(Color("blue"))
+                           
+                    })
+                    .padding(.leading, -160)
+                    .fullScreenCover(isPresented: $isPrsnt){
+                        Settings()
+                    }
+                    Spacer()
+                      
                         
-                    }.position(x: 40 , y: 30)
+                        
+                    
+                    
+                    
+                    
                 }
+               
+                
+                
                 VStack{
                     Text(mainText).font(.title).foregroundColor(Color("blue")).bold().padding().position(x: 200 ,y: 200)
-                    Text(text).font(.title).foregroundColor(Color("blue")).bold().padding().position(x: 200, y: 300)
+                    Text(text).font(.title).foregroundColor(Color("blue")).bold().padding().position(x: 200, y: 2)
                     
                     
                 }
             }
             
-        }   .onAppear(perform: {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 5){
-                text = "4 things you can touch"
-                
-            }
-        })
-        .onAppear(perform: {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 10){
-                text =  "3 things you can hear"
-                
-            }
-        })
-        .onAppear(perform: {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 15){
-                text = "2 things you can smell"
-                
-            }
-        })
-        .onAppear(perform: {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 20){
-                text = "1 thing you can taste"
-                
-            }
-        })
-        .onAppear(perform: {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 25){
-                mainText = ""
-                text = "You did great!"
-                
-            }
-        })
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5){
+                    text = "4 things you can touch"
+                    
+                }
+            })
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 10){
+                    text =  "3 things you can hear"
+                    
+                }
+            })
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 15){
+                    text = "2 things you can smell"
+                    
+                }
+            })
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 20){
+                    text = "1 thing you can taste"
+                    
+                }
+            })
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 25){
+                    mainText = ""
+                    text = "You did great!"
+                    
+                }
+            })
+        }
     }
 }
 
